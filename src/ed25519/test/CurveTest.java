833fc85e99afccc72ea3398edd8e248c4a7fe82a
BjornArnelid <bjorn.arnelid@alten.se>
2013-12-19 15:48:12 +0100

    Changed Assert methods in CurveTest to TestNG Asserts

--- src/ed25519/test/CurveTest.java
+++ src/ed25519/test/CurveTest.java
@@ -2,7 +2,7 @@ package ed25519.test;
 
 import java.math.BigInteger;
 
-import org.testng.AssertJUnit;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -20,43 +20,43 @@ public class CurveTest {
 	@Test(dependsOnGroups = {"expmod", "getxx"}, groups = {"invert"})
 	public void testInvert1() {
 		BigInteger expected = BigInteger.valueOf(1);
-		AssertJUnit.assertEquals(expected, c.invert(BigInteger.valueOf(1)));
+		Assert.assertEquals(c.invert(BigInteger.valueOf(1)), expected);
 	}
 	
 	@Test(dependsOnGroups = {"expmod", "getxx"}, groups = {"invert"})
 	public void testInvert5() {
 		BigInteger expected = new BigInteger("11579208923731619542357098500868790785326998466564056403945758400791312963990");
-		AssertJUnit.assertEquals(expected, c.invert(BigInteger.valueOf(5)));
+		Assert.assertEquals(c.invert(BigInteger.valueOf(5)), expected);
 	}
 	
 	@Test(dependsOnGroups = {"expmod"}, groups = {"getxx"})
 	public void testgetXXfor0() {
 		BigInteger expected = BigInteger.valueOf(-1);
-		AssertJUnit.assertEquals(expected, c.getXX(BigInteger.valueOf(0)));
+		Assert.assertEquals(c.getXX(BigInteger.valueOf(0)), expected);
 	}
 	
  	@Test(dependsOnGroups = {"expmod"}, groups = {"getxx"})
 	public void testgetXXfor1() {
 		BigInteger expected = BigInteger.valueOf(0);
-		AssertJUnit.assertEquals(expected, c.getXX(BigInteger.valueOf(1)));
+		Assert.assertEquals(c.getXX(BigInteger.valueOf(1)), expected);
 	}
  	
  	@Test(dependsOnGroups = {"expmod"}, groups = {"getxx"})
 	public void testgetXXfor2() {
 		BigInteger expected = new BigInteger("159238947029881800007354471772647911766795077324544932017472991350137141588898");
-		AssertJUnit.assertEquals(expected, c.getXX(BigInteger.valueOf(2))); 		
+		Assert.assertEquals(c.getXX(BigInteger.valueOf(2)), expected); 		
  	}
 
 	@Test(dependsOnGroups = {"expmod", "getxx"}, groups = {"recoverx"})
 	public void testXrecoverEven() {
 		BigInteger expected = new BigInteger("19681161376707505956807079304988542015446066515923890162744021073123829784752");
-		AssertJUnit.assertEquals(expected, c.recoverX(BigInteger.valueOf(0)));
+		Assert.assertEquals(c.recoverX(BigInteger.valueOf(0)), expected);
 	}
 	
 	@Test(dependsOnGroups = {"expmod", "getxx"}, groups = {"recoverx"})
 	public void testXrecoverOdd() {
 		BigInteger expected = new BigInteger("26193273134124080442446118532604303931175156347221147955160486408041496074798");
-		AssertJUnit.assertEquals(expected, c.recoverX(BigInteger.valueOf(4)));
+		Assert.assertEquals(c.recoverX(BigInteger.valueOf(4)), expected);
 	}
 
 	@Test(dependsOnGroups = {"bigpoint", "invert", "recoverx"}, groups = {"basepoint"})
@@ -65,7 +65,7 @@ public class CurveTest {
 		BigInteger y = new BigInteger("46316835694926478169428394003475163141307993866256225615783033603165251855960");
 		BigPoint expected = new BigPoint(x,y);
 		BigPoint basePoint = c.getBasePoint();
-		AssertJUnit.assertEquals(expected, basePoint);
+		Assert.assertEquals(basePoint, expected);
 	}
 	
 	@Test(dependsOnGroups = {"bigpoint"}, groups = {"edward"})
@@ -74,14 +74,23 @@ public class CurveTest {
 		BigPoint second = new BigPoint(new BigInteger("0"), new BigInteger("0"));
 		BigPoint expected = new BigPoint(new BigInteger("0"), new BigInteger("0"));
 		BigPoint result = c.edwards(first, second);
-		AssertJUnit.assertEquals(expected, result);
+		Assert.assertEquals(result, expected);
+	}
+	
+	@Test(dependsOnGroups = {"bigpoint"}, groups = {"edward"})
+	public void testEdwards1and1() {
+		BigPoint first = new BigPoint(new BigInteger("1"), new BigInteger("1"));
+		BigPoint second = new BigPoint(new BigInteger("1"), new BigInteger("1"));
+		BigPoint expected = new BigPoint(new BigInteger("243332"), new BigInteger("51380297829790456491237162401597246033761091082941378702394490295467508647106"));
+		BigPoint result = c.edwards(first, second);
+		Assert.assertEquals(result, expected);
 	}
 
 	@Test(dependsOnGroups = {"bigpoint"})
 	public void testScalarMult0() {
 		BigPoint expected = new BigPoint(new BigInteger("0"), new BigInteger("1"));
 		BigPoint result = c.scalarmult(new BigInteger("0"));
-		AssertJUnit.assertEquals(expected, result);
+		Assert.assertEquals(result, expected);
 	}
 	
 //	public void testScalarMult1() {
