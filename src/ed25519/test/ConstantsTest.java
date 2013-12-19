5cad004c917dbaaa3649fa9cb4c1d9c526b6285c
BjornArnelid <bjorn.arnelid@gmail.com>
2013-12-19 20:18:42 +0100

    Migrated ConstantsTest to testNG asserts

--- src/ed25519/test/ConstantsTest.java
+++ src/ed25519/test/ConstantsTest.java
@@ -2,7 +2,7 @@ package ed25519.test;
 
 import java.math.BigInteger;
 
-import org.testng.AssertJUnit;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -19,12 +19,12 @@ public class ConstantsTest {
 	@Test
 	public void testGetBitLength() {
 		int expected = 256;
-		AssertJUnit.assertEquals(expected, c.getb());
+		Assert.assertEquals(c.getb(), expected);
 	}
 
 	@Test
 	public void testBigPrime() {
 		BigInteger expected = new BigInteger("57896044618658097711785492504343953926634992332820282019728792003956564819949");
-		AssertJUnit.assertEquals(expected, c.getq());
+		Assert.assertEquals(c.getq(), expected);
 	}	
 }
